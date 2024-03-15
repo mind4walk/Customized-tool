892361d11d45089236e6cad3d16b617e3e435fad
mind4walk <mai4work@gamil.com>
2024-03-15 07:11:16 +0000

    Optimize run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,63 @@
 #!/bin/bash
-PATH_TO_VIMRC=/etc/vim/vimrc
-COLO_NAME=bla
 
-# Vim settings
-cat <<EOL >> $PATH_TO_VIMRC
+# Configuration variables, true=enable.
+#centos=true
+ubuntu=true
+add_tmux=true
+apply_vim_settings=true
+apply_vimdiff_colors=true
+
+# Function to install tmux
+add_tmux() {
+	local os_install_command=""
+	if [[ "$ubuntu" == "true" ]]; then
+		os_install_command="apt"
+	else
+		os_install_command="yum"
+	fi
+	if [[ -n "$os_install_command" ]]; then
+		sudo $os_install_command install tmux -y
+	fi
+}
+
+# Function to apply Vim settings
+apply_vim_settings() {
+    VIMRC_PATH="/etc/vim/vimrc"
+    cat <<EOL >> "$VIMRC_PATH"
 set cursorline
 
-" Color configuration - albert 
+" Color configuration - A.W.L.
 set bg=dark
 color evening  " Same as :colorscheme evening
 hi LineNr cterm=bold ctermfg=DarkGrey ctermbg=NONE
 hi CursorLineNr cterm=bold ctermfg=Green ctermbg=NONE
 EOL
+}
 
-# Vimdiff colors
-COLO_PATH=$(find / -type d -name "vim[0-9]*" 2>/dev/null -exec find {} -type d -name "colors" \;)
-sleep 1
-cat << EOF > $COLO_PATH/$COLO_NAME.vim
+# Function to apply Vimdiff colors
+apply_vimdiff_colors() {
+    COLO_NAME="bla"
+    COLO_FILENAME="$COLO_NAME.vim"
+
+    # Function to find vim colors directory
+    find_vim_colors() {
+		local vim_dirs=($(find / -type d -name "vim[0-9]*" 2>/dev/null))
+		for dir in "${vim_dirs[@]}"; do
+			if [[ -d "$dir/colors" ]]; then
+				echo "$dir/colors"
+				return 0
+			fi
+		done
+		echo "Error: Unable to find Vim colors directory" >&2
+		return 1
+	}
+
+	local COLO_PATH=$(find_vim_colors)
+	if [[ -z "$COLO_PATH" ]]; then
+	    exit 1
+	fi
+
+	cat << EOF > "$COLO_PATH/$COLO_FILENAME"
 " Re-writer: A.W.L.
 hi clear Normal
 set bg&
@@ -30,3 +71,19 @@ highlight DiffDelete cterm=bold ctermfg=black ctermbg=grey gui=none guifg=bg gui
 highlight DiffChange cterm=bold ctermfg=black ctermbg=grey gui=none guifg=bg guibg=Red
 highlight DiffText   cterm=bold ctermfg=black ctermbg=darkred gui=none guifg=bg guibg=Red
 EOF
+}
+
+execute_task() {
+	if [[ "$1" == "true" ]]; then
+		echo "*"
+		echo "* $2"
+		echo "********************"
+		shift 2
+		"$@"
+    fi
+}
+
+# Main function
+execute_task "$add_tmux" "Install tmux ..." add_tmux
+execute_task "$apply_vim_settings" "Modify vim settings ..." apply_vim_settings
+execute_task "$apply_vimdiff_colors" "Add vimddiff colors ..." apply_vimdiff_colors
